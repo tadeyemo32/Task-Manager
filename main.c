e45de30137baf3a94cb8d56a18008f5e805a3a41
tadeyemo32 <tadeyemo32@gmail.com>
2025-06-09 16:42:37 +0800

    Task data strcture created and malloced

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define titleSize 100 
+#define descriptionSize 255
+
+typedef struct Task {
+
+int id; // Taks ID number for separation 
+int state ; // task completed or not completed 
+char title[titleSize]; // Name of task
+char description[descriptionSize];//Task information 
+
+}Task;
+
+int createTask(int id,char description[descriptionSize]  ,char title[titleSize]){
+
+Task* newTask = malloc(sizeof(Task));
+
+if (!newTask){
+    printf("Memory Allocation failed\n");
+}
+
+
+    return 0; 
+}
+
+
+
+
+
+
+
+
+int main(){
+char man[descriptionSize] = "Head to the gym at 10am";
+char mum[titleSize] = "Gym";
+
+    createTask(10000,man,mum);
+
+    return 0;
+}
